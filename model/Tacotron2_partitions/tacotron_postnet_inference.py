9c1a3174dc631a093a19d42344d65432ff1dc8bd
Ali Sheykh <sheykha8@gmail.com>
2024-07-14 21:14:34 +0330

    added postent dockerfile and server for postnet network

---
+++ model/Tacotron2_partitions/tacotron_postnet_inference.py
@@ -0,0 +1,20 @@
+import torch
+from model.Tacotron2_partitions.tacotron_post_net import TacotronPostNet
+
+
+postnet = TacotronPostNet()
+postnet.load_state_dict(
+    torch.load("model/Tacotron2_partitions/model_store/tacotron_postnet_weights.pt")
+)
+postnet.eval()
+
+
+def get_inference(mel_output, alignments):
+    # we need to convert input lengths and encoded inputs to tensors before continuing the process:
+    mel_output = torch.tensor(mel_output)
+    alignments = torch.tensor(mel_output)
+    mel_outputs_postnet, alignments = postnet(
+        mel_output, alignments
+    )
+
+    return mel_outputs_postnet, alignments
