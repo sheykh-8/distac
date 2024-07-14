2950103fe0342f94447efab089b4d0f5c66234cc
Ali Sheykh <sheykha8@gmail.com>
2024-07-15 00:49:30 +0330

    added hifigan server and dockerfile

---
+++ model/Tacotron2_partitions/hifigan_inference.py
@@ -0,0 +1,22 @@
+import torch
+from model.Tacotron2_partitions.hifigan import HifiganGenerator
+import torchaudio
+import uuid
+
+vocoder = HifiganGenerator()
+vocoder.load_state_dict(
+    torch.load("model/Tacotron2_partitions/model_store/hifigan_weights.pt")
+)
+vocoder.eval()
+
+
+def get_inference(spectrogram):
+    # we need to convert input lengths and encoded inputs to tensors before continuing the process:
+    spectrogram = torch.tensor(spectrogram)
+    waveform = vocoder(
+        spectrogram
+    )
+
+    audio_path = f"inferenced_audio{uuid.uuid4().hex}.wav"
+    torchaudio.save(audio_path, waveform.squeeze(1), 22050)
+    return audio_path
---
+++ model/hifigan_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, send_file, request
+from model.Tacotron2_partitions.hifigan_inference import get_inference
+import os
+
+app = Flask(__name__)
+
+@app.route('/generate-audio', methods=['POST'])
+def handler():
+    data = request.get_json()
+
+    spectrogram = data.get('spectrogram')
+
+    if spectrogram is None:
+        return {"error": "No spectrogram provided"}, 400
+
+    audio_path = get_inference(spectrogram)
+
+    # Send the file and then delete it
+    response = send_file(audio_path, as_attachment=True, attachment_filename='generated_audio.wav')
+    
+    @response.call_on_close
+    def remove_file():
+        try:
+            os.remove(audio_path)
+        except Exception as e:
+            app.logger.error(f"Error removing file {audio_path}: {e}")
+    
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
