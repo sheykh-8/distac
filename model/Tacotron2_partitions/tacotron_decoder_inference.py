97ff37f846a05be1a732936316421b6ee8b9d517
Ali Sheykh <sheykha8@gmail.com>
2024-07-15 02:41:00 +0330

    fixed an issue with the decoder container

--- model/Tacotron2_partitions/tacotron_decoder_inference.py
+++ model/Tacotron2_partitions/tacotron_decoder_inference.py
@@ -12,7 +12,9 @@ decoder.eval()
 def get_inference(encoded_inputs, input_lengths):
     # we need to convert input lengths and encoded inputs to tensors before continuing the process:
     encoded_inputs = torch.tensor(encoded_inputs)
-    input_lengths = torch.tensor(encoded_inputs)
+    print(f"encoded input shape {encoded_inputs.shape}")
+    input_lengths = torch.tensor(input_lengths)
+    print(f"input length shape {input_lengths.shape}")
     mel_outputs, gate_outputs, alignments, mel_lengths = decoder(
         encoded_inputs, input_lengths
     )
